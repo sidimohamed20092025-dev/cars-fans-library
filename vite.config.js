e5f72e449d4a454ec41494854b925cab710293db
sidimohamed20092025-dev <sidimohamed20092025@gmail.com>
2026-09-16 17:01:02 +0100

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  root: 'client',
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true
+      }
+    }
+  },
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true
+  }
+});
